69aa23322db548da39973d1918f361d27b4696fe
laiaknosse <yazevv97@yandex.ru>
2023-06-22 04:50:44 +0500

    SpringBeans

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,11 +1,13 @@
 package app.model;
 
+
 import org.springframework.stereotype.Component;
 
-@Component("cat")
-public class Cat extends Animal {
+@Component("dog")
+public class Dog extends Animal {
+
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "i'm a Dog";
     }
 }
